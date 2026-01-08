cbc1fcc386af97e12ed14091798c48d542a13ee9
heeeete <mkoui@naver.com>
2026-01-08 19:57:16 +0900

    chore(eslint): unused-imports 및 simple-import-sort 플러그인 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
-import unusedImports from 'eslint-plugin-unused-imports';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -10,7 +11,10 @@ const eslintConfig = defineConfig([
 
   {
     plugins: {
+      // 사용하지 않는 import 방지
       'unused-imports': unusedImports,
+      // import 정렬
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       // 중복 리포트 방지(권장)
@@ -30,17 +34,31 @@ const eslintConfig = defineConfig([
           argsIgnorePattern: '^_',
         },
       ],
+
+      // import 정렬 설정:
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // Packages `react` related packages come first.
+            ['^react', '^@?\\w'],
+            // Internal packages.
+            ['^(@|components)(/.*|$)'],
+            // Side effect imports.
+            ['^\\u0000'],
+            // Parent imports. Put `..` last.
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            // Other relative imports. Put same-folder imports and `.` last.
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+            // Style imports.
+            ['^.+\\.?(css)$'],
+          ],
+        },
+      ],
     },
   },
-  // Override default ignores of eslint-config-next.
 
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-  ]),
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
   eslintConfigPrettier,
 ]);
 
